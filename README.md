93dd883a8720e44114cc8558bbdc46015efc43cf
Marek Kwasecki <marek@kwasecki.com>
2018-06-21 12:54:13 +0200

    NEW  - creds from config  - logging intensively if started with --verbose  - common part (client object) is not created very time a call is made  - thus simplified configuration
    ISSUE: Unable to connect to endpoint still...

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
-# platform-orthanc-s3-storage
+# orthanc-s3-storage
 
 Orthanc plugin providing AWS S3 storage
+
+# Instruction, how to build the plugin
+
+## Prerequisits
+1. Build AWS-SDK from scratch (optional)
+  - `git clone https://github.com/aws/aws-sdk-cpp.git`
+  - `cmake -DCMAKE_INSTALL_PREFIX=install -DBUILD_ONLY=s3 -H aws-sdk-cpp -B aws-sdk-cpp/build && cmake --build aws-sdk-cpp/build -- -j4`
+  - I didn't `make install`, so I need to set paths maually later
+  - 1GB of RAM is not enough. Adding 4GB swap file helped.
+
+2. Build orthanc
+  - TODO:
+  - use script provided with the plugin from scripts folder
+
+## Build just a plugin
+  - use script provided with the plugin from scripts folder
+  - copy `libOrthancS3StoragePlugin.dylib*` or `libOrthancS3StoragePlugin.so*` to the folder indicated in Orthanc's json config file
+
+## Full build of Orthanc
+
+If you follow this way, you can build Orthanc with QtCreator. Then you can debug all the stuff.
+
+1. update main `CMakeLists.txt` of Orthanc to include new plugin (file `CMakeLists.txt-main-level` inside `sss` directory)
+2. build whole orthanc (remember flag `-j` of `make`!) with `cmake`. If you get into troubles, check [Orthanc's manual](http://book.orthanc-server.com/faq/compiling.html). 
+3. the plugins appears in `Orthanc_build` directory
+
--- src/Plugin.cpp
+++ src/Plugin.cpp
@@ -22,10 +22,14 @@
 #include "OrthancPluginCppWrapper.h"
 
 #include <aws/core/Aws.h>
+#include <aws/core/auth/AWSCredentialsProvider.h>
 #include <aws/s3/S3Client.h>
 #include <aws/s3/model/PutObjectRequest.h>
 #include <aws/s3/model/DeleteObjectRequest.h>
 #include <aws/s3/model/GetObjectRequest.h>
+
+#include <aws/core/utils/logging/DefaultLogSystem.h>
+#include <aws/core/utils/logging/AWSLogging.h>
 
 #include <json/value.h>
 #include <json/reader.h>
@@ -36,23 +40,27 @@
 
 #include <boost/interprocess/streams/bufferstream.hpp>
 
+namespace OrthancPlugins {
+
 bool UploadFileToS3(const std::string & path, const void* content, const int64_t& size);
 bool DownloadFileFromS3(const std::string & path, void** content, int64_t* size);
-bool DeleteFileAtS3(const std::string & path);
+bool DeleteFileFromS3(const std::string & path);
 
+#define ALLOCATION_TAG "Orthanc S3 Storage"
 
-static OrthancPluginContext* context_ = NULL;
-static std::string storageDir_ = "";
-static std::string indexDir_ = "";
+static OrthancPluginContext* context = NULL;
+static std::string indexDir = "";
 
-static std::string bucket_name_ = "";
-static std::string region_ = "";
+static std::string s3_bucket_name = "orthanc-s3-storage";
+static std::string s3_region  = "eu-central-1";
 
-
+std::shared_ptr<Aws::S3::S3Client> s3_client;
+Aws::SDKOptions aws_api_options;
+std::mutex mutex;  // protects g_i
 
 static std::string GetPath(const char* uuid)
 {
-  return std::string(uuid);
+    return std::string(uuid);
 }
 
 static std::string GetPathStorage(const char* uuid)
@@ -62,7 +70,7 @@ static std::string GetPathStorage(const char* uuid)
 
 static std::string GetPathInstance(const char* uuid)
 {
-  return indexDir_ + "/" + std::string(uuid);
+    return indexDir + "/" + std::string(uuid);
 }
 
 
@@ -77,8 +85,6 @@ static OrthancPluginErrorCode StorageCreate(const char* uuid,
   FILE* fp = nullptr;
   switch (type) {
   case OrthancPluginContentType_Dicom:
-      //NOTE credentials are set in ~/.aws/credentials file
-      //TODO make this correct way, get credentials form orthanc's json file
       path   = GetPathStorage(uuid);
       ok = UploadFileToS3(path, content, size)==EXIT_SUCCESS;
       break;
@@ -95,9 +101,8 @@ static OrthancPluginErrorCode StorageCreate(const char* uuid,
       break;
   default:
       ok = false;
-      std::cerr << "Never should happen.\n";
+      OrthancPluginLogError(context, "Never should happen");
   }
-  //return OrthancPluginErrorCode_Success;
   return ok ? OrthancPluginErrorCode_Success : OrthancPluginErrorCode_StorageAreaPlugin;
 }
 
@@ -136,14 +141,15 @@ static OrthancPluginErrorCode StorageRead(void** content,
         if (*size == 0) {
           *content = NULL;
         } else {
-          *content = malloc(*size);
+          //*content = malloc(*size);
+          *content = (void*) new char[*size];
           ok = !(*content == NULL || fread(*content, *size, 1, fp) != 1);
         }
         fclose(fp);
         break;
     default:
-        std::cerr << "Never should happen.\n";
-        path   = GetPath(uuid);
+        OrthancPluginLogError(context, "Never should happen");
+        ok = false;
     }
     return ok ? OrthancPluginErrorCode_Success : OrthancPluginErrorCode_StorageAreaPlugin;
 }
@@ -157,85 +163,77 @@ static OrthancPluginErrorCode StorageRemove(const char* uuid,
     switch (type) {
     case     OrthancPluginContentType::OrthancPluginContentType_Dicom:
         path   = GetPathStorage(uuid);
-        ok =  DeleteFileAtS3(path)==EXIT_SUCCESS;
+        ok =  DeleteFileFromS3(path)==EXIT_SUCCESS;
         break;
     case OrthancPluginContentType_DicomAsJson:
         path   = GetPathInstance(uuid);
         ok = (remove(path.c_str()) == 0);
         break;
     default:
-        std::cerr << "Never should happen.\n";
-        path   = GetPath(uuid);
+        OrthancPluginLogError(context, "Never should happen");
+        ok = false;
     }
     return ok ? OrthancPluginErrorCode_Success: OrthancPluginErrorCode_StorageAreaPlugin;
 }
 
-bool UploadFileToS3(const std::string & path, const void* content, const int64_t& size){
+bool UploadFileToS3(const std::string & path, const void* content, const int64_t& size) {
+    std::lock_guard<std::mutex> lock(mutex);
 
-Aws::SDKOptions options;
-Aws::InitAPI(options);
-{
-    const Aws::String bucket_name = bucket_name_.c_str();
-    const Aws::String region = region_.c_str();
-    const Aws::String key_name = path.c_str();
-    const Aws::String file_name = path.c_str();
+    std::string s("[S3] PUT: ");
+    s += path;
+    OrthancPluginLogInfo(context, s.c_str());
 
-    std::cout << "Uploading " << file_name << " to S3 bucket " <<
-        bucket_name << " at key " << key_name << std::endl;
-
-    Aws::Client::ClientConfiguration clientConfig;
-    if (!region.empty())
-        clientConfig.region = region;
-    Aws::S3::S3Client s3_client(clientConfig);
-
-    Aws::S3::Model::PutObjectRequest object_request;
-    object_request.WithBucket(bucket_name).WithKey(key_name);
-
-    std::shared_ptr<Aws::IOStream> body =  std::shared_ptr<Aws::IOStream>(new boost::interprocess::bufferstream((char*)content, size));
-
-    auto input_data = Aws::MakeShared<Aws::FStream>("PutObjectInputStream",
-             file_name.c_str(), std::ios_base::in | std::ios_base::binary);
-
-
-    object_request.SetBody(body);
-
-    auto put_object_outcome = s3_client.PutObject(object_request);
-
-    if (put_object_outcome.IsSuccess())
     {
-        std::cout << "Done!" << std::endl;
+        const Aws::String bucket_name = s3_bucket_name.c_str();
+        const Aws::String key_name = path.c_str();
+        const Aws::String file_name = path.c_str();
+
+        Aws::S3::Model::PutObjectRequest object_request;
+        object_request.WithBucket(bucket_name).WithKey(key_name);
+
+        std::shared_ptr<Aws::IOStream> body =  std::shared_ptr<Aws::IOStream>(new boost::interprocess::bufferstream((char*)content, size));
+
+        auto input_data = Aws::MakeShared<Aws::FStream>("PutObjectInputStream",
+                                                        file_name.c_str(), std::ios_base::in | std::ios_base::binary);
+
+
+        object_request.SetBody(body);
+
+        auto put_object_outcome = s3_client->PutObject(object_request);
+
+        if (put_object_outcome.IsSuccess())
+        {
+            OrthancPluginLogInfo(context, "[S3] Done!");
+        }
+        else
+        {
+            std::string s("[S3] PUT error: ");
+            s += put_object_outcome.GetError().GetExceptionName() + " ";
+            s += put_object_outcome.GetError().GetMessage();
+            OrthancPluginLogError(context, s.c_str());
+
+            return EXIT_FAILURE;
+        }
     }
-    else
-    {
-        std::cout << "PutObject error: " <<
-            put_object_outcome.GetError().GetExceptionName() << " " <<
-            put_object_outcome.GetError().GetMessage() << std::endl;
-    }
-}
-Aws::ShutdownAPI(options);
-return EXIT_SUCCESS;
+
+    return EXIT_SUCCESS;
 }
 
 bool DownloadFileFromS3(const std::string & path, void** content, int64_t* size){
-    Aws::SDKOptions options;
-    Aws::InitAPI(options);
+    std::lock_guard<std::mutex> lock(mutex);
+
+    std::string s("[S3] GET: ");
+    s += path;
+    OrthancPluginLogInfo(context, s.c_str());
+
     {
-        const Aws::String bucket_name = bucket_name_.c_str();
-        const Aws::String region = region_.c_str();
+        const Aws::String bucket_name = s3_bucket_name.c_str();
         const Aws::String key_name = path.c_str();
-
-        std::cout << "Downloading " << key_name << " from S3 bucket: " <<
-            bucket_name << std::endl;
-
-        Aws::Client::ClientConfiguration clientConfig;
-        if (!region.empty())
-            clientConfig.region = region;
-        Aws::S3::S3Client s3_client(clientConfig);
 
         Aws::S3::Model::GetObjectRequest object_request;
         object_request.WithBucket(bucket_name).WithKey(key_name);
 
-        auto get_object_outcome = s3_client.GetObject(object_request);
+        auto get_object_outcome = s3_client->GetObject(object_request);
 
         bool ok = false;
         if (get_object_outcome.IsSuccess())
@@ -243,136 +241,186 @@ bool DownloadFileFromS3(const std::string & path, void** content, int64_t* size)
             *size = get_object_outcome.GetResult().GetContentLength();
             Aws::OStringStream buf;
             buf << get_object_outcome.GetResult().GetBody().rdbuf();
-            *content = malloc(*size);
+            //*content = malloc(*size);
+            *content = new char[*size];
             if (*content!=nullptr) {
                 memcpy(*content, buf.str().c_str(), *size);
             } else {
-                Aws::ShutdownAPI(options);
                 return EXIT_FAILURE;
             }
-            std::cout << "Done!" << std::endl;
+            OrthancPluginLogInfo(context, "[S3] Done!");
         } else {
-            std::cout << "GetObject error: " <<
-                get_object_outcome.GetError().GetExceptionName() << " " <<
-                get_object_outcome.GetError().GetMessage() << std::endl;
+            std::string s("[S3] GET error: ");
+            s += get_object_outcome.GetError().GetExceptionName() + " ";
+            s += get_object_outcome.GetError().GetMessage();
+            OrthancPluginLogError(context, s.c_str());
+
+            return EXIT_FAILURE;
         }
     }
 
-    Aws::ShutdownAPI(options);
     return EXIT_SUCCESS;
 }
 
-bool DeleteFileAtS3(const std::string & path) {
-    Aws::SDKOptions options;
-    Aws::InitAPI(options);
+bool DeleteFileFromS3(const std::string & path) {
+    std::lock_guard<std::mutex> lock(mutex);
+
+    std::string s("[S3] DELETE: ");
+    s += path;
+    OrthancPluginLogInfo(context, s.c_str());
+
     {
+        const Aws::String bucket_name = s3_bucket_name.c_str();
+        const Aws::String key_name = path.c_str();
 
-    const Aws::String bucket_name = bucket_name_.c_str();
-    const Aws::String region = region_.c_str();
-    const Aws::String key_name = path.c_str();
+        Aws::S3::Model::DeleteObjectRequest object_request;
+        object_request.WithBucket(bucket_name).WithKey(key_name);
 
-    std::cout << "Deleting" << key_name << " from S3 bucket: " <<
-                bucket_name << std::endl;
+        auto delete_object_outcome = s3_client->DeleteObject(object_request);
 
-    Aws::Client::ClientConfiguration clientConfig;
-    if (!region.empty())
-        clientConfig.region = region;
-    Aws::S3::S3Client s3_client(clientConfig);
+        if (delete_object_outcome.IsSuccess())
+        {
+            OrthancPluginLogInfo(context, "[S3] Done!");
+        }
+        else
+        {
+            std::string("[S3] DELETE error: ");
+            s += delete_object_outcome.GetError().GetExceptionName() + " ";
+            s += delete_object_outcome.GetError().GetMessage();
+            OrthancPluginLogError(context, s.c_str());
 
-
-    Aws::S3::Model::DeleteObjectRequest object_request;
-    object_request.WithBucket(bucket_name).WithKey(key_name);
-
-    auto delete_object_outcome = s3_client.DeleteObject(object_request);
-
-    if (delete_object_outcome.IsSuccess())
-    {
-        std::cout << "Done!" << std::endl;
+            return EXIT_FAILURE;
+        }
     }
-    else
-    {
-        std::cout << "DeleteObject error: " <<
-            delete_object_outcome.GetError().GetExceptionName() << " " <<
-            delete_object_outcome.GetError().GetMessage() << std::endl;
-    }
-    }
-    Aws::ShutdownAPI(options);
+
     return EXIT_SUCCESS;
 }
 
 
+extern "C" {
 
-extern "C"
+ORTHANC_PLUGINS_API int32_t OrthancPluginInitialize(OrthancPluginContext* c)
 {
-  ORTHANC_PLUGINS_API int32_t OrthancPluginInitialize(OrthancPluginContext* c)
-  {
-    context_ = c;
-    OrthancPluginLogWarning(context_, "S3 Storage plugin is initializing");
+    context = c;
+    OrthancPluginLogWarning(context, "[S3] Storage plugin is initializing");
 
     /* Check the version of the Orthanc core */
     if (OrthancPluginCheckVersion(c) == 0)
     {
-      char info[1024];
-      sprintf(info, "Your version of Orthanc (%s) must be above %d.%d.%d to run this plugin",
-              c->orthancVersion,
-              ORTHANC_PLUGINS_MINIMAL_MAJOR_NUMBER,
-              ORTHANC_PLUGINS_MINIMAL_MINOR_NUMBER,
-              ORTHANC_PLUGINS_MINIMAL_REVISION_NUMBER);
-      OrthancPluginLogError(context_, info);
-      return -1;
+        char info[1024];
+        sprintf(info, "Your version of Orthanc (%s) must be above %d.%d.%d to run this plugin",
+                c->orthancVersion,
+                ORTHANC_PLUGINS_MINIMAL_MAJOR_NUMBER,
+                ORTHANC_PLUGINS_MINIMAL_MINOR_NUMBER,
+                ORTHANC_PLUGINS_MINIMAL_REVISION_NUMBER);
+        OrthancPluginLogError(context, info);
+        return -1;
     }
 
+    OrthancPluginSetDescription(context, "Implementation of S3 Storage.");
 
-    OrthancPlugins::OrthancConfiguration configuration(context_);
+    OrthancPlugins::OrthancConfiguration configuration(context);
+    //not needed?
+    indexDir = configuration.GetStringValue("IndexDirectory", indexDir.c_str());  //last parameter = default value
 
-    OrthancPlugins::OrthancConfiguration s3Storage;
-    configuration.GetSection(s3Storage, "Storage_AWS_S3");
-
-
-    //StorageDirectory not needed
-    //storageDir_ = configuration.GetStringValue("StorageDirectory", "");
-    //if (storageDir_.empty()) {
-    //    OrthancPluginLogWarning(context_, "StorageDir was not set. Using Orthanc's defaults.");
-    //}
-
-    indexDir_ = configuration.GetStringValue("IndexDirectory", "");  //last parameter = default value
-    if (indexDir_.empty()) {
-        //TODO maybe force Orthanc to crash? (i.e. return 1)
-        OrthancPluginLogWarning(context_, "IndexDirectory was not set. Using current working directory for index.");
-        indexDir_ = ".";
+    if (indexDir.empty()) {
+        OrthancPluginLogWarning(context, "StorageDir and IndexDir were not set.");
     }
 
-    bucket_name_ = s3Storage.GetStringValue("bucket_name", "");
-    region_  = s3Storage.GetStringValue("aws_region", "");
-    if (bucket_name_.empty() || region_.empty()) {
-        OrthancPluginLogWarning(context_, "S3 credentials were not set. S3 plugin not enabled");
-        //NOTE prevent Orthanc to start by changing return value to 1
-        return 0;
+    // obtain S3 creds
+    OrthancPlugins::OrthancConfiguration s3_configuration(context);
+    if (configuration.IsSection("S3")) {
+        configuration.GetSection(s3_configuration, "S3");
+    } else {
+        OrthancPluginLogError(context, "Can't find `S3` section in the config.");
+        return EXIT_FAILURE;
     }
 
-    OrthancPluginRegisterStorageArea(context_, StorageCreate, StorageRead, StorageRemove);
+    //required
+    std::string s3_access_key;
+    if (!s3_configuration.LookupStringValue(s3_access_key, "aws_access_key_id")) {
+        OrthancPluginLogError(context, "AWS credentials were not set.");
+        return EXIT_FAILURE;
+    }
+
+    //required
+    std::string s3_secret_key;
+    if (!s3_configuration.LookupStringValue(s3_secret_key, "aws_secret_access_key")) {
+        OrthancPluginLogError(context, "AWS credentials were not set.");
+        return EXIT_FAILURE;
+    }
+
+    std::string region;
+    if (s3_configuration.LookupStringValue(region, "aws_region")) {
+        s3_region = region.c_str();
+    }
+
+    std::string bucket;
+    if (s3_configuration.LookupStringValue(bucket, "s3_bucket")) {
+        s3_bucket_name = bucket.c_str();
+    }
+
+    //Initialization of AWS SDK
+    aws_api_options.loggingOptions.logLevel = Aws::Utils::Logging::LogLevel::Trace;
+
+    Aws::InitAPI(aws_api_options);
+
+    //Enable AWS logging
+    Aws::Utils::Logging::InitializeAWSLogging(
+        Aws::MakeShared<Aws::Utils::Logging::DefaultLogSystem>(
+            ALLOCATION_TAG, Aws::Utils::Logging::LogLevel::Trace, "aws_"));
+
+    //aws_client_config.region = s3_region;
+    Aws::Client::ClientConfiguration aws_client_config;
+    aws_client_config.region = Aws::Region::EU_CENTRAL_1;
+    aws_client_config.scheme = Aws::Http::Scheme::HTTPS;
+    aws_client_config.connectTimeoutMs = 30000;
+    aws_client_config.requestTimeoutMs = 600000;
+
+    s3_client = Aws::MakeShared<Aws::S3::S3Client>(
+        ALLOCATION_TAG, Aws::Auth::AWSCredentials(s3_access_key, s3_secret_key), aws_client_config);
+
+    auto outcome = s3_client->ListBuckets();
+    if (outcome.IsSuccess()) {
+        auto buckets = outcome.GetResult().GetBuckets();
+        for (auto i : buckets) {
+            std::cout << "Bucket: " << i.GetName() << '\n';
+        }
+        std::cout << std::endl;
+    } else {
+        std::string s("[S3] List Buckets error: ");
+        s += outcome.GetError().GetExceptionName() + " ";
+        s += outcome.GetError().GetMessage();
+        OrthancPluginLogError(context, s.c_str());
+    }
+
+    OrthancPluginRegisterStorageArea(context, StorageCreate, StorageRead, StorageRemove);
+
     return 0;
-  }
-
-
-
-
-
-  ORTHANC_PLUGINS_API void OrthancPluginFinalize()
-  {
-    OrthancPluginLogWarning(context_, "S3 Storage plugin is finalizing");
-  }
-
-
-  ORTHANC_PLUGINS_API const char* OrthancPluginGetName()
-  {
-    return "s3-storage";
-  }
-
-
-  ORTHANC_PLUGINS_API const char* OrthancPluginGetVersion()
-  {
-    return "1.0";
-  }
 }
 
+
+ORTHANC_PLUGINS_API void OrthancPluginFinalize()
+{
+    //Cleanup AWS logging
+    Aws::Utils::Logging::ShutdownAWSLogging();
+    Aws::ShutdownAPI(aws_api_options);
+
+    OrthancPluginLogWarning(context, "[S3] Storage plugin is finalizing");
+}
+
+
+ORTHANC_PLUGINS_API const char* OrthancPluginGetName()
+{
+    return "s3-storage";
+}
+
+
+ORTHANC_PLUGINS_API const char* OrthancPluginGetVersion()
+{
+    return "1.0";
+}
+
+} //extern C
+
+} //namespace
