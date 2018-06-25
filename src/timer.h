1b6a18c28affe3aa7f2c3e53fd241e0793945d8f
Marek Kwasecki <marek@kwasecki.com>
2018-06-25 12:03:33 +0200

    NEW  - add utils with filessytem operations  - add timer for measuring operation time  - refactor Plugin.cpp a bit

---
+++ src/timer.h
@@ -0,0 +1,35 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <boost/core/noncopyable.hpp>
+
+#include <chrono>
+#include <type_traits>
+#include <iostream>
+
+namespace OrthancPlugins {
+
+template <typename Clock = std::chrono::high_resolution_clock>
+    class stopwatch
+    {
+        const typename Clock::time_point start_point;
+    public:
+        stopwatch() :
+            start_point(Clock::now())
+        {}
+
+        template <
+            typename Units = typename std::chrono::microseconds,
+            typename Rep = typename Clock::duration::rep>
+        Rep elapsed() const
+        {
+            std::atomic_thread_fence(std::memory_order_relaxed);
+            auto counted_time = std::chrono::duration_cast<Units>(Clock::now() - start_point);
+            std::atomic_thread_fence(std::memory_order_relaxed);
+            return counted_time.count();
+        }
+    };
+
+}
+
+#endif // TIMER_H
